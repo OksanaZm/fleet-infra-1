7b255902a1e3b11a5a8bf318ef6e9aa219474789
OksanaZm <ksushakrz@gmail.com>
2025-07-07 10:50:21 +0200

    thresholds set to 0

--- datadog-terraform/imagebackoff_monitor.tf
+++ datadog-terraform/imagebackoff_monitor.tf
@@ -1,15 +1,15 @@
 resource "datadog_monitor" "image_pull_backoff" {
-  name    = "K8s ImagePullBackOff Alert"
+  name    = "Kubernetes ImagePullBackOff"
   type    = "query alert"
   query   = <<EOT
-avg(last_5m):max:kubernetes_state.container.waiting{reason:ImagePullBackOff} by {kube_container_name} > 0
+avg(last_5m):max:kubernetes_state.container.waiting{reason:ImagePullBackOff} by {pod} > 0
 EOT
 
-  message  = "ImagePullBackOff detected on container {{kube_container_name.name}}"
-  tags     = ["k8s", "imagepullbackoff"]
+  message  = "ImagePullBackOff detected on pod {{pod.name}}"
   priority = 3
+  tags     = ["k8s", "image", "alert"]
 
-  thresholds {
-    critical = 1
+  monitor_thresholds {
+    critical = 0
   }
 }
