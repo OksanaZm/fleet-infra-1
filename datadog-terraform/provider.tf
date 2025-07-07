e05333d99a54f91d454db22108f8854f28b58ab2
OksanaZm <ksushakrz@gmail.com>
2025-07-07 10:58:14 +0200

    helm chart for datadog alerts

--- datadog-terraform/provider.tf
+++ datadog-terraform/provider.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    datadog = {
+      source  = "datadog/datadog"
+      version = "~> 3.39"
+    }
+  }
+}
 provider "datadog" {
   api_key = var.datadog_api_key
   app_key = var.datadog_app_key
+  api_url = "https://api.datadoghq.eu"
 }
